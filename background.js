88326f9f02c9e5c842d3e2127c5c12ffe40a2e01
baharalidurrani <baharalidurrani@gmail.com>
2020-05-10 07:02:41 -0700

    global added

--- background.js
+++ background.js
@@ -1,12 +1,26 @@
-chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-  if (msg.action === "updateIcon") {
-    // if (msg.value) {
-    chrome.browserAction.setIcon({ path: "/icons/green.png" });
-    // } else {
-    //     chrome.browserAction.setIcon({path: "/assets/cross.png"});
-    // }
+// chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+//   if (msg.action === "updateIcon") {
+//     // if (msg.value) {
+//     // chrome.browserAction.setIcon({ path: "/icons/green.png" });
+//     // } else {
+//     //     chrome.browserAction.setIcon({path: "/assets/cross.png"});
+//     // }
+//   }
+// });
+// chrome.webNavigation.onBeforeNavigate.addListener(function () {
+//   chrome.browserAction.setIcon({ path: "/icons/red.png" });
+// });
+
+chrome.webNavigation.onCompleted.addListener(function (details) {
+  for (let i = 0; i < siteData.length; i++) {
+    if (details.url.includes(siteData[i].host)) {
+      chrome.browserAction.setIcon({ path: "/icons/green.png" });
+      console.log("greeeeeeeeen");
+      break;
+    } else {
+      chrome.browserAction.setIcon({ path: "/icons/red.png" });
+      console.log("reeeeeeeeeeeeeeeeed");
+    }
   }
-});
-chrome.webNavigation.onBeforeNavigate.addListener(function () {
-  chrome.browserAction.setIcon({ path: "/icons/red.png" });
+  console.log("details...................", details);
 });
---
+++ global.js
@@ -0,0 +1,76 @@
+var siteData = [
+  {
+    host: "www.google.com",
+    tours: [
+      {
+        link:
+          "https://baharalidurrani.gitlab.io/resume/assets/dep/js/tours/googleBoot.js",
+        name: "Something Google Tour"
+      },
+      {
+        link:
+          "https://baharalidurrani.gitlab.io/resume/assets/dep/js/tours/googleBoot2.js",
+        name: "Other Google Tour"
+      },
+      {
+        link:
+          "https://baharalidurrani.gitlab.io/resume/assets/dep/js/tours/googleBoot3.js",
+        name: "Last Google Tour"
+      }
+    ]
+  },
+  {
+    host: "baharalidurrani.me",
+    tours: [
+      {
+        link:
+          "https://baharalidurrani.gitlab.io/resume/assets/dep/js/tours/baharBoot.js",
+        name: "Something Tour Durrani"
+      },
+      {
+        link:
+          "https://baharalidurrani.gitlab.io/resume/assets/dep/js/tours/baharBoot2.js",
+        name: "Other Tour Bahar"
+      },
+      {
+        link: "https://pensive-davinci-b144ff.netlify.app/hubby.js",
+        name: "Last Tour Ryan"
+      }
+    ]
+  },
+  {
+    host: "prosto-visa.com.ua",
+    tours: [
+      {
+        link:
+          "https://tender-noether-fe0a8b.netlify.app/dep/js/tours/prostoBoot.js",
+        name: "Something Tour Durrani"
+      },
+      {
+        link:
+          "https://tender-noether-fe0a8b.netlify.app/dep/js/tours/prostoBoot2.js",
+        name: "Other Tour Bahar"
+      }
+    ]
+  },
+  {
+    host: "127.0.0.1:5500",
+    tours: [
+      {
+        link:
+          "https://baharalidurrani.gitlab.io/resume/assets/dep/js/tours/baharBoot.js",
+        name: "Something Tour Name"
+      },
+      {
+        link:
+          "https://baharalidurrani.gitlab.io/resume/assets/dep/js/tours/baharBoot2.js",
+        name: "Other Tour Name"
+      },
+      {
+        link:
+          "https://baharalidurrani.gitlab.io/resume/assets/dep/js/tours/baharBoot3.js",
+        name: "Last Tour Name"
+      }
+    ]
+  }
+];
